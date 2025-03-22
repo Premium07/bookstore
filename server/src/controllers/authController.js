4c8ae8cacf59df248c5b356e2f1d3426f0e461c4
Premium07 <premsagarg23@gmail.com>
2025-03-22 16:40:13 +0545

    Implement user signup with validation, token generation, and password hashing

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,5 +1,54 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+};
+
 export const signup = async (req, res) => {
-  res.send("signup Page");
+  try {
+    const { email, username, password } = req.body;
+
+    if (!email || !username || !password)
+      return res.status(400).json({ message: "All fields are required" });
+
+    if (password.length < 6)
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+
+    if (username.length < 3)
+      return res
+        .status(400)
+        .json({ message: "Username must be at least 3 characters long" });
+
+    // check if user already exists
+
+    const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
+
+    // get random avatar
+    const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+    const user = new User({ email, username, password, profileImage });
+    await user.save();
+
+    const token = generateToken(user._id);
+
+    res.status(201).json({
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        profileImage: user.profileImage,
+      },
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 export const login = async (req, res) => {
   res.send("login Page");
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
       required: true,
+      unique: true,
     },
     email: {
       type: String,
@@ -23,6 +25,18 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// hash the passwod before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error); // Pass error to avoid breaking the process
+  }
+});
 
 const User = mongoose.model("User", userSchema);
 
