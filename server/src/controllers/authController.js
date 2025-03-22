f785f140197b89d581bdc2e856b0b4e9798faf6f
Premium07 <premsagarg23@gmail.com>
2025-03-22 14:07:30 +0545

    Add MongoDB connection, user and book models, and authentication routes

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,6 @@
+export const signup = async (req, res) => {
+  res.send("signup Page");
+};
+export const login = async (req, res) => {
+  res.send("login Page");
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,20 @@
-import express from "express";
-const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
+import { connectDB } from "./config/db.config.js";
+
+const app = express();
 const PORT = process.env.PORT || 8080;
+connectDB();
 
 // middlewares
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.get("/", (req, res) => {
-  res.send("Hello from server");
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/book", bookRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
