e9bda189ce41053b9dc436448eae606c345055d3
Premium07 <premsagarg23@gmail.com>
2025-03-23 12:45:06 +0545

    Add authentication check to book creation route

--- server/src/routes/bookRoutes.js
+++ server/src/routes/bookRoutes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { createBook } from "../controllers/bookController.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 const router = Router();
 
-router.post("/create", createBook);
+router.post("/create", isAuthenticated, createBook);
 
 export default router;
