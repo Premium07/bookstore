279c018099db92dbef00aa75b3496995b7d987b3
Premium07 <premsagarg23@gmail.com>
2025-03-29 16:57:36 +0545

    Add root route to server: respond with "Hello World!" for base URL

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
+
 app.use("/api/auth", authRoutes);
 app.use("/api/book", bookRoutes);
+
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
