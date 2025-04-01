b7c74c36a1343e36afdfbaf143a6985df3b4bcfc
Premium07 <premsagarg23@gmail.com>
2025-04-01 17:13:48 +0545

    Add tab navigation layout and screens for Home, Create, and Profile

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,15 +1,34 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import "./globals.css";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
+import { useAuthStore } from "../store/authStore";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+
+  // console.log("segment:", segments);
+  const { checkAuth, user, token } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    const inAuthScreen = segments[0] === "(auth)";
+    const isSignedIn = user && token;
+    if (!inAuthScreen && !isSignedIn) router.replace("/(auth)");
+    else if (inAuthScreen && isSignedIn) router.replace("/(tabs)");
+  }, [user, token, segments]);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
         <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="(auth)" />
         </Stack>
       </SafeScreen>
